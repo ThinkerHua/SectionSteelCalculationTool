63e64f356d993b62d765d8e60142ee7934c6388a
ThinkerHua <thinkerhua@hotmail.com>
2024-12-13 15:44:57 +0800

    代码文件添加版权声明

--- SectionSteelCalculationTool/Program.cs
+++ SectionSteelCalculationTool/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*==============================================================================
+ *  SectionSteelCalculationTool - A tool that assists Excel in calculating 
+ *  quantities of steel structures
+ *
+ *  Copyright © 2022 Huang YongXing.                 
+ *
+ *  This library is free software, licensed under the terms of the GNU 
+ *  General Public License as published by the Free Software Foundation, 
+ *  either version 3 of the License, or (at your option) any later version. 
+ *  You should have received a copy of the GNU General Public License 
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ *==============================================================================
+ *  Program.cs: entrance of main form
+ *  written by Huang YongXing - thinkerhua@hotmail.com
+ *==============================================================================*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
