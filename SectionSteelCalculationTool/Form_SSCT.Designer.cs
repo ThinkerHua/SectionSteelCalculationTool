6f3ace643198cb09d6f9fb03436611d8c70eac87
ThinkerHua <thinkerhua@hotmail.com>
2024-12-13 15:44:57 +0800

    代码文件添加版权声明

--- SectionSteelCalculationTool/Form_SSCT.Designer.cs
+++ SectionSteelCalculationTool/Form_SSCT.Designer.cs
@@ -1,4 +1,18 @@
-﻿
+﻿/*==============================================================================
+ *  SectionSteelCalculationTool - A tool that assists Excel in calculating 
+ *  quantities of steel structures
+ *
+ *  Copyright © 2024 Huang YongXing.                 
+ *
+ *  This library is free software, licensed under the terms of the GNU 
+ *  General Public License as published by the Free Software Foundation, 
+ *  either version 3 of the License, or (at your option) any later version. 
+ *  You should have received a copy of the GNU General Public License 
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ *==============================================================================
+ *  Form_SSCT.Designer.cs: form designer for user interface
+ *  written by Huang YongXing - thinkerhua@hotmail.com
+ *==============================================================================*/
 namespace SectionSteelCalculationTool {
     partial class Form_SSCT {
         /// <summary>
